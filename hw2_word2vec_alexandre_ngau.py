466d541dd2377c2a2a25e96e11c66a7c9d36cc66
alexandrengau <145470229+alexandrengau@users.noreply.github.com>
2023-11-02 18:27:20 +0100

    Update hw2_word2vec_alexandre_ngau.py

--- hw2_word2vec_alexandre_ngau.py
+++ hw2_word2vec_alexandre_ngau.py
@@ -278,7 +278,7 @@ def training(model, batch_size, n_epochs, lr=5e-5):
             # Backward pass
             loss_positive = torch.mean(criterion(output_positive, torch.ones(output_positive.shape, device=DEVICE)), dim=1)
             loss_negative = torch.mean(criterion(output_negative, torch.zeros(output_negative.shape, device=DEVICE)), dim=1)
-            print(f"loss positive = {loss_positive}")
+            print(f"loss = {torch.mean(loss_positive+loss_negative)}")
             loss = loss_positive.item() + loss_negative.item()
             loss.backward()
             optimizer.step()
