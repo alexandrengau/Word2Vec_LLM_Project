6883a7a6f5024abdfa40e6c0ca6c809f32489b55
alexandrengau <145470229+alexandrengau@users.noreply.github.com>
2023-11-02 16:51:37 +0100

    Update hw2_word2vec_alexandre_ngau.py

--- hw2_word2vec_alexandre_ngau.py
+++ hw2_word2vec_alexandre_ngau.py
@@ -36,7 +36,7 @@ print(DEVICE)
 
 R = 4
 K = 6
-batch_size = 512
+batch_size = 128
 n_epochs = 10
 
 """First cells will be the same than the ones of the lab on text convolution.
@@ -89,7 +89,7 @@ You should:
 
 """
 
-n_samples = 5000  # the number of training example
+n_samples = 1000  # the number of training example
 
 # We first shuffle the data !
 dataset = dataset.shuffle()
